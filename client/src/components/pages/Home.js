e0ce88e9620b5c76a40c5c0983944d2282e1ce99
ben4code <nnaemeka.obioha@gmail.com>
2020-12-02 10:55:40 +0000

    Added apollo/client and getPosts query to clientside

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-export default function Home() {
+
+
+export default function Home() {   
+  const { loading, data } = useQuery(FETCH_POST_QUERY);
+  console.log(loading, data)
+
+  
   return (
     <div>
       <h1>Home</h1>
     </div>
-  )
+  );
 }
+
+
+const FETCH_POST_QUERY = gql`
+    {
+      getPosts {
+        id
+        body
+        createdAt
+        username
+        likeCount
+        likes {
+          username
+        }
+        commentCount
+        comments {
+          id
+          username
+          createdAt
+          body
+        }
+      }
+    }
+  `;
--- src/server.js
+++ src/server.js
@@ -1,32 +1,35 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const { ApolloServer, PubSub } = require("apollo-server");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
-
-
-dotenv.config()
+dotenv.config();
 
 const pubSub = new PubSub();
 
 const server = new ApolloServer({
+  cors:  {
+    origin: "*",
+  },
   typeDefs,
   resolvers,
-  context: ({req}) => ({req, pubSub})
-})
+  context: ({ req }) => ({ req, pubSub })
+});
 
-mongoose.connect(process.env.MONGODBURI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-}).then(()=> {
-  console.log("Database is running.")
-})
+mongoose
+  .connect(process.env.MONGODBURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database is running.");
+  });
 
-server.listen('5000').then(()=> {
+server.listen().then(() => {
   console.log(`
     Server is running!
     Listening on port 5000
-  `)
+  `);
 });
